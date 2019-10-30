001f73c5a3243ead77252121a8432a644c121e20
yadavarjun1996 <57184501+yadavarjun1996@users.noreply.github.com>
2019-10-30 21:52:32 +0530

    Update fact.c

--- fact.c
+++ fact.c
@@ -1,15 +1,17 @@
 #include<stdio.h>
-int main(){
-int i,n=8;
-unsigned long int factorial=1;
-if(n<0)
+int main()
+{
+ int i,n=8;
+ unsigned long int factorial=1;
+ if(n<0)
     {printf("invalid number");}
-else {
-        for(i=n;i>=1;i--)
-        {
-           factorial=factorial*i;
-         }
-     }
+ else {
+        for(i=2;i<=n;i++)
+       {
+          factorial=factorial*i;
+       }
+      }
 printf("factorial=%lu",factorial);
 return 0;
 }
+
