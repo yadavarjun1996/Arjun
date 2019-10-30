05ead24feb203e4720ba07e55b64e4b8bf7f6ec2
yadavarjun1996 <57184501+yadavarjun1996@users.noreply.github.com>
2019-10-30 21:40:46 +0530

    Update fact.c

--- fact.c
+++ fact.c
@@ -5,10 +5,11 @@ unsigned long int factorial=1;
 if(n<0)
     {printf("invalid number");}
 else {
-    for(i=n;i>=1;i--){
-    factorial=factorial*i;
-    }
-}
+        for(i=n;i>=1;i--)
+        {
+           factorial=factorial*i;
+         }
+     }
 printf("factorial=%lu",factorial);
 return 0;
 }
